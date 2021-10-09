cb5f7fd2bec3bdbd4ba0381232f1da69d0fe33d8
Kurt Barry <kurt.m.barry@gmail.com>
2021-10-09 13:32:37 -0400

    test for poke and associated functions

--- src/CurveLPOracle.t.sol
+++ src/CurveLPOracle.t.sol
@@ -5,6 +5,10 @@ import "ds-test/test.sol";
 
 import "./CurveLPOracle.sol";
 
+interface Hevm {
+    function warp(uint256) external;
+}
+
 contract MockCurvePool {
     uint256 public get_virtual_price;
     address[] public coins;
@@ -21,14 +25,26 @@ contract MockCurvePool {
 }
 
 contract MockOracle {
+    uint256 private price;
+    function read() external view returns (uint256) {
+        return price;
+    }
+    function setPrice(uint256 _price) external {
+        price = _price;
+    }
 }
 
 contract CurveLpOracleTest is DSTest {
+    uint256 constant WAD = 10**18;
+    uint256 constant DEFAULT_HOP = 3600;  // 1 hour in seconds
+
+    Hevm hevm;
     MockCurvePool pool;
     address[] orbs;
     CurveLPOracle oracle;
 
     function setUp() public {
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
         pool = new MockCurvePool();
         pool.addCoin(address(0x1));
         pool.addCoin(address(0x2));
@@ -37,6 +53,7 @@ contract CurveLpOracleTest is DSTest {
         orbs.push(address(new MockOracle()));
         orbs.push(address(new MockOracle()));
         oracle = new CurveLPOracle(address(pool), "123CRV", orbs);
+        oracle.step(DEFAULT_HOP);
     }
 
     function test_constructor_and_public_fields() public {
@@ -101,5 +118,58 @@ contract CurveLpOracleTest is DSTest {
 
     function testFail_link_invalid_index() public {
         oracle.link(oracle.ncoins(), address(0x42));
+    }
+
+    function test_poke_zph_zzz_pass_peep_peek_read() public {
+        oracle.kiss(address(this));
+
+        MockOracle(orbs[0]).setPrice(100 * WAD);
+        MockOracle(orbs[1]).setPrice(102 * WAD);
+        MockOracle(orbs[2]).setPrice( 98 * WAD);
+        pool.setVirtualPrice(105_831 * WAD / 10**5);  // 1.05831
+
+        uint256 hop = oracle.hop();
+
+        hevm.warp(oracle.zph());
+        assertTrue(oracle.pass());
+
+        oracle.poke();
+
+        assertEq(oracle.zph(), block.timestamp + hop);
+        assertEq(oracle.zzz(), block.timestamp);
+        assertTrue(!oracle.pass());
+
+        (bytes32 val, bool has) = oracle.peep();
+        assertTrue(has);
+        uint256 firstPrice = (98 * WAD) * pool.get_virtual_price() / WAD;  // minimum price used to value all assets
+        assertEq(uint256(val), firstPrice);
+
+        hevm.warp(oracle.zph() - 1);  // warp to just before the next possible poke
+        assertTrue(!oracle.pass());
+
+        hevm.warp(oracle.zph() + hop / 2);  // warp somewhat beyond next possible poke
+        assertTrue(oracle.pass());
+
+        // update values
+        MockOracle(orbs[0]).setPrice(101 * WAD);
+        MockOracle(orbs[1]).setPrice(105 * WAD);
+        MockOracle(orbs[2]).setPrice(103 * WAD);
+        pool.setVirtualPrice(106_792 * WAD / 10**5);  // 1.06792
+
+        oracle.poke();
+
+        assertEq(oracle.zph(), block.timestamp + hop);
+        assertEq(oracle.zzz(), block.timestamp);
+        assertTrue(!oracle.pass());
+
+        (val, has) = oracle.peek();
+        assertTrue(has);
+        assertEq(uint256(val), firstPrice);
+        assertEq(oracle.read(), val);
+
+        (val, has) = oracle.peep();
+        assertTrue(has);
+        uint256 secondPrice = (101 * WAD) * pool.get_virtual_price() / WAD;  // minimum price used to value all assets
+        assertEq(uint256(val), secondPrice);
     }
 }
